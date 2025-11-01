902f55aff5860be847369e2ec701960be31092c7
chintannayaka7-hub <chintannayaka7@gmail.com>
2025-11-01 11:49:03 +0530

    js_basic.js

---
+++ js_basic.js
@@ -0,0 +1,12 @@
+// console.log("Hello, encode");
+
+ let namee = "Chintan";
+ let age = 18;
+
+console.log(namee,age)
+console.table({namee,age})
+
+let a = 2;
+let b = "2";
+console.log(a == b);
+console.log(a === b);
