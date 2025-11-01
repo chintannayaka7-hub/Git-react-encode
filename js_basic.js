adc95d454f412c7a11477d1d6ac46db790ab698d
chintannayaka7-hub <chintannayaka7@gmail.com>
2025-11-01 14:43:13 +0530

    Git-react-encode

--- js_basic.js
+++ js_basic.js
@@ -1,12 +1,31 @@
 // console.log("Hello, encode");
 
- let namee = "Chintan";
- let age = 18;
+// console,log(" the namee " , namee , "and" , age , )
+// console.log(`my namee is ${namee} and age is ${age}`)
 
-console.log(namee,age)
-console.table({namee,age})
+// let newStr = `my namee is ${namee} and age is ${age}`
+// console.log("newSter : " , newStr) 
 
-let a = 2;
-let b = "2";
-console.log(a == b);
-console.log(a === b);
+ let helllo = function(){
+
+    console.log("hello");
+ }
+ console.log(helllo())
+let obj = {
+    namee : "chintan",
+    surnamee : "nayaka",
+    fun : () => {
+        console.log("kemcho")
+    }
+}
+let obj1 = {
+    namee : "Black",
+    surnamee : "clover"
+
+}
+let arr = [obj ,obj1]
+console.log(arr[0].namee)
+// obj.fun()
+
+ let {namee} = obj1
+ console.log(namee)
